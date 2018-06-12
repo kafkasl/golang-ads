2c6eabd37a81a5ef7908686198875e6c1849bd6a
Pol.Alvarez@BSC <pol.avms@gmail.com>
2018-06-12 11:00:38 +0200

    added test/examples script

---
+++ bin/run_tests_and_examples.sh
@@ -0,0 +1,24 @@
+# Test union-find
+cd ../structures/union-find
+go test
+cd -
+
+# Test trie
+cd ../structures/trie
+go test
+cd -
+
+# Test patricia-trie
+cd ../structures/patricia-trie
+go test
+cd -
+
+# Test skip-list
+cd ../structures/skip-list
+go test
+cd -
+
+# Test frequent-itemsets
+cd ../structures/frequent-itemsets
+go test
+cd -
